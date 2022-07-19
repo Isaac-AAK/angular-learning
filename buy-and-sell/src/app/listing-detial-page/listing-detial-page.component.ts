247aff9667cfe4b3071f8eb637b86a04d16c4a3e
Isaac <isaac@mail.com>
2022-07-19 08:08:37 -0400

    listing component

--- buy-and-sell/src/app/listing-detial-page/listing-detial-page.component.ts
+++ buy-and-sell/src/app/listing-detial-page/listing-detial-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
+
 
 @Component({
   selector: 'app-listing-detial-page',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing-detial-page.component.css']
 })
 export class ListingDetialPageComponent implements OnInit {
-
-  constructor() { }
+ 
+  listing: Listing;
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id ===id);
   }
 
 }
